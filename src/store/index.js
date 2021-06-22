020511fd5bf66987472509149da91a71ecc9037d
Davion <daviongarcia@gmail.com>
2021-06-22 00:09:21 -0700

    setup/wiring

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import products from '../products.js';
+import products from './products.js';
 import cart from './cart.js';
 
 const reducers = combineReducers( {products, cart} );
