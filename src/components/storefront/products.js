eb37eb6878f43cdf856d7bd7c44123ab5694b634
Davion <daviongarcia@gmail.com>
2021-06-22 00:06:01 -0700

    setup/wiring

--- src/components/storefront/products.js
+++ src/components/storefront/products.js
@@ -1,28 +1,21 @@
 import {useDispatch, useSelector} from 'react-redux';
+import {getPorducts} from "../../store/products";
 
-import {} from "../../store/products";
-// import * as actions from "./store/counter.js"; // actions.increment
 
 
 function getProd(props) {
 
   const dispatch = useDispatch();
-  const count = useSelector( (state) => state.products.products )
+  const products = useSelector( (state) => state.products.products )
 
   function get() {
-    dispatch( increment() )
+    dispatch( getPorducts() )
   }
-
-
 
   return (
     <>
-      <h2>EZ Counter: {count}</h2>
-      <button onClick={subtractOne}>-</button>
-      <button onClick={addOne}>+</button>
     </>
   )
 
 }
 
-export default Counter;
