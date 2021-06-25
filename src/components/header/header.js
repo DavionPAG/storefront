2ff61cc546e65ac510780560e57f71ea83c67698
Davion <daviongarcia@gmail.com>
2021-06-24 23:43:16 -0700

    add more stuffs

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  fontFamily: 'Chakra Petch',
+
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 export default function Header() {
-  return(
+
+  const classes = useStyles();
+
+  return (
+
     <header>
-      <h1>Our Store</h1>
-      <a href="http://">Cart</a>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <span variant="h6" className={classes.title}>
+              The Re-Lax Shop
+            </span>
+            <Button color="inherit">Cart</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
     </header>
   )
 }
+
+
--- src/components/storefront/products.js
+++ src/components/storefront/products.js
@@ -1,19 +1,55 @@
-import {useDispatch, useSelector} from 'react-redux';
-import {getPorducts} from "../../store/products";
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from "../../store/products";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Details from '../../components/products/details.js'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
-
-function getProd(props) {
+export default function ShowProducts (props) {
 
   const dispatch = useDispatch();
-  const products = useSelector( (state) => state.products.products )
+  const products = useSelector((state) => state.products.currentProducts)
+  
+  const classes = useStyles();
 
-  function get() {
-    dispatch( getPorducts() )
+  function showToys() {
+    dispatch(actions.prodAction('toys'))
+  }
+
+  function showGames() {
+    dispatch(actions.prodAction('games'))
   }
 
   return (
     <>
+      <Grid container className={classes.root} spacing={2}>
+        <div>
+          <button onClick={showToys}>Toys</button>
+          <span> | </span>
+          <button onClick={showGames}>Games</button>
+        </div>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            {products.map((data, idx) => (
+              <Grid key={idx} item>
+                <Details product={data}/>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   )
 
